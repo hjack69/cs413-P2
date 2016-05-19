1b074c6b526ed73ae1d477678c1aee2213cf1ac5
Jack <jackbrobst@nau.edu>
2016-05-19 12:37:28 -0700

    Initial commit containing all needed files and some skeleton code.

---
+++ Game.js
@@ -0,0 +1,19 @@
+/**
+ * Created by jack on 5/19/16.
+ */
+
+// Main graphics variables
+var gameport = document.getElementById("gameport");
+var renderer = PIXI.autoDetectRenderer(WIDTH, HEIGHT, {backgroundColor: 0});
+gameport.appendChild(renderer.view);
+var stage = new PIXI.Container();
+
+function update() {
+
+}
+
+function main() {
+
+}
+
+main();
