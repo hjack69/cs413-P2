0e35c444c4837be58986bb310888ccce544e1423
Jack <jackbrobst@nau.edu>
2016-05-21 18:43:11 -0700

    Added images

--- Game.js
+++ Game.js
@@ -2,6 +2,9 @@
  * Created by jack on 5/19/16.
  */
 
+const WIDTH = 1000;
+const HEIGHT = 600;
+
 // Main graphics variables
 var gameport = document.getElementById("gameport");
 var renderer = PIXI.autoDetectRenderer(WIDTH, HEIGHT, {backgroundColor: 0});
